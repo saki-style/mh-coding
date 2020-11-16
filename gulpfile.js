178bcff722278e422a4c33bb7c9a2fbb09bd3999
saki <73070512+sk-style@users.noreply.github.com>
2020-11-16 22:02:05 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,22 @@
-const gulp = require("gulp");
+const gulp = require('gulp');
 
 /* sass */
-const sass = require("gulp-sass");
-const plumber = require("gulp-plumber");
-const notify = require("gulp-notify");
-const sassGlob = require("gulp-sass-glob");
-const mmq = require("gulp-merge-media-queries");
-const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
-const cssdeclsort = require("css-declaration-sorter");
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
+const sassGlob = require('gulp-sass-glob');
+const mmq = require('gulp-merge-media-queries');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssdeclsort = require('css-declaration-sorter');
 
 /* browser-sync */
-const browserSync = require("browser-sync");
+const browserSync = require('browser-sync');
 
 /* imagemin */
-const imagemin = require("gulp-imagemin");
-const imageminPngquant = require("imagemin-pngquant");
-const imageminMozjpeg = require("imagemin-mozjpeg");
+const imagemin = require('gulp-imagemin');
+const imageminPngquant = require('imagemin-pngquant');
+const imageminMozjpeg = require('imagemin-mozjpeg');
 const imageminOption = [
 	imageminPngquant({ quality: [0.65, 0.8] }),
 	imageminMozjpeg({ quality: 85 }),
@@ -31,45 +31,45 @@ const imageminOption = [
 ];
 
 
-gulp.task("sass", function() {
+gulp.task('sass', function() {
 	return gulp
-		.src("./sass/**/*.scss")
-		.pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
+		.src('./sass/**/*.scss')
+		.pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
 		.pipe(sassGlob())
-		.pipe(sass({ outputStyle: "expanded" }))
+		.pipe(sass({ outputStyle: 'expanded' }))
 		.pipe(postcss([autoprefixer()]))
-		.pipe(postcss([cssdeclsort({ order: "alphabetical" })]))
+		.pipe(postcss([cssdeclsort({ order: 'alphabetical' })]))
 		.pipe(mmq())
-		.pipe(gulp.dest("./css"));
+		.pipe(gulp.dest('./css'));
 });
 
-gulp.task("watch", function(done) {
-	gulp.watch("./sass/**/*.scss", gulp.task("sass"));
-	gulp.watch("./sass/**/*.scss", gulp.task("bs-reload"));
-	gulp.watch("./js/*.js", gulp.task("bs-reload"));
-	gulp.watch("./*.html", gulp.task("bs-reload"));
+gulp.task('watch', function(done) {
+	gulp.watch('./sass/**/*.scss', gulp.task('sass'));
+	gulp.watch('./sass/**/*.scss', gulp.task('bs-reload'));
+	gulp.watch('./js/*.js', gulp.task('bs-reload'));
+	gulp.watch('./*.html', gulp.task('bs-reload'));
 });
 
-gulp.task("browser-sync", function(done) {
+gulp.task('browser-sync', function(done) {
 	browserSync.init({
 		server: {
-			baseDir: "./",
-			index: "index.html"
+			baseDir: './',
+			index: 'index.html'
 		}
 	});
 	done();
 });
 
-gulp.task("bs-reload", function(done) {
+gulp.task('bs-reload', function(done) {
 	browserSync.reload();
 	done();
 });
 
-gulp.task("imagemin", function() {
+gulp.task('imagemin', function() {
 	return gulp
-		.src("./img/base/*.{png,jpg,gif,svg}")
+		.src('./img/base/*.{png,jpg,gif,svg}')
 		.pipe(imagemin(imageminOption))
-		.pipe(gulp.dest("./img"));
+		.pipe(gulp.dest('./img'));
 });
 
-gulp.task("default", gulp.series(gulp.parallel("browser-sync", "watch")));
+gulp.task('default', gulp.series(gulp.parallel('browser-sync', 'watch')));
